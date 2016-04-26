615e69b64dc5c8fbf652b4ed10928d3bbf5ac367
ys305751572 <305751572@qq.com>
2016-04-26 16:59:05 +0800

    11

--- src/main/java/com/gcs/aol/controller/h5/MoneyPayController.java
+++ src/main/java/com/gcs/aol/controller/h5/MoneyPayController.java
@@ -2,6 +2,7 @@ package com.gcs.aol.controller.h5;
 
 import com.gcs.aol.constant.Constant;
 import com.gcs.aol.service.IMoneyMagDodManager;
+import com.gcs.pay.util.XMLUtil;
 import com.gcs.utils.Result;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -14,11 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 /**
@@ -41,6 +45,69 @@ public class MoneyPayController {
         Map<String,Object> resultParam = moneyMagDodManager.payConfig(request,response,dodId,type);
         return Result.success(resultParam,"payConfig");
     }
+
+    @RequestMapping(value="notify/weixinmoney", method = RequestMethod.POST)
+    public void notifyWeixinBorrow(HttpServletRequest request) {
+        Map<String,Object> result = parse(request);
+        String status = result.get("result_code").toString();
+        if ("SUCCESS".equals(status)) {
+            String sn = result.get("out_trade_no").toString();
+
+            try {
+//                BorrowRepayRecord record = new BorrowRepayRecord();
+//                record.setSn(sn);
+//                record.setAmount(Integer.parseInt(result.get("total_fee").toString()) / 100.0);
+//
+//                borrowService.repayFinish(record);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping(value="notify/alipaymoney", method = RequestMethod.POST)
+    public void notifyAlipayBorrow(HttpServletRequest request) {
+        String status = request.getParameter("trade_status").toString();
+        String sn = request.getParameter("out_trade_no").toString();
+
+        System.out.println("status:" + status + "==sn:" + sn);
+        double amount = Double.parseDouble(request.getParameter("total_fee").toString());
+        System.out.println("total_fee:" + amount);
+
+        if("TRADE_SUCCESS".equals(status) || "FINISH".equals(status)) {
+            System.out.println("SUCCESS");
+            try {
+//                BorrowRepayRecord record = new BorrowRepayRecord();
+//                record.setSn(sn);
+//                record.setAmount(amount);
+//
+//                borrowService.repayFinish(record);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String,Object> parse(HttpServletRequest request) {
+        Map<String,Object> resultMap = null;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            String line = null;
+            String result = "";
+            while ((line = reader.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("result:" + result);
+            resultMap = XMLUtil.doXMLParse(result);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultMap;
+    }
+
     /*********************************************************************************************
      *  暂留
      *********************************************************************************************/
